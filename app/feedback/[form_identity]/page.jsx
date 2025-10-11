8d7da43c6db3630eb0320a44c1ef491547bb4c45
dalienst <oduordalienst@gmail.com>
2025-10-11 17:37:24 +0300

    update background

--- app/feedback/[form_identity]/page.jsx
+++ app/feedback/[form_identity]/page.jsx
@@ -133,7 +133,7 @@ function Feedback({ params }) {
   if (isLoadingFeedbackForm) return <LoadingSpinner />;
 
   return (
-    <div className="flex items-center justify-center min-h-screen py-4 px-4 bg-background">
+    <div className="flex items-center justify-center min-h-screen py-4 px-4 bg-gradient-to-b from-[#e54c23] via-[#2a2d7b] to-[#e20715]">
       <div className="w-full max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow-md">
         <Image
           className="mx-auto mb-4"
