e239efc49ed4fddc633c2fc97a908c4c68282f46
Dalienst Owino <88655199+dalienst@users.noreply.github.com>
2026-02-12 14:26:44 +0300

    Increase items per page from 5 to 10

--- components/postings/PostingsTable.jsx
+++ components/postings/PostingsTable.jsx
@@ -40,7 +40,7 @@ function PostingsTable({ postings, refetch }) {
     posting_type: "",
   });
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5;
+  const itemsPerPage = 10;
 
   // Handle filter input changes
   const handleFilterChange = (e) => {
