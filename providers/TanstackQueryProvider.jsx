7929675cf1147de8dee95546c89b481a615d3e90
Dalienst <oduordalienst@gmail.com>
2026-09-13 15:48:50 +0300

    fix speed

--- providers/TanstackQueryProvider.jsx
+++ providers/TanstackQueryProvider.jsx
@@ -2,10 +2,21 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import React from "react";
+import React, { useState } from "react";
 
 function TanstackQueryProvider({ children }) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000, // Data remains fresh for 1 minute
+            refetchOnWindowFocus: false, // Prevent redundant background fetches on focus
+            retry: 1,
+          },
+        },
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
